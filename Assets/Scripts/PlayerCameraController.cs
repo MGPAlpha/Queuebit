94155457b1c93fea91af78a0905d0596b009b5eb
MGPAlpha <mgpalpha1@gmail.com>
2020-04-09 17:56:54 -0400

    Revert "Develop rhythm"

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class PlayerCameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector3 target;
+    public Transform target;
     public float smoothTime = 0.2f;
-    public Vector3 offset = new Vector3(0, 0, -1);
+    public Vector3 offset = new Vector3(0, 10, 0);
     private Vector3 velocity = Vector3.zero;
     
     void FixedUpdate() {
         if (target != null) {
-            Vector3 desiredPosition = target + offset;
+            Vector3 desiredPosition = target.position + offset;
             Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
             transform.position = smoothedPosition;
         }
     }
 
-    public void setCameraTarget(Vector3 inputTarget) {
+    public void setCameraTarget(Transform inputTarget) {
         target = inputTarget;
     }
 
